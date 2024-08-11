80a131702bf0eb7409a7bf85c475423600893395
bignyokki <shiro5989@yahoo.co.jp>
2024-08-12 04:17:12 +0900

    firebaseでデプロイできるようになった

---
+++ src/utils/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCfhrV1ELZyFq8AZS65oiGBQZDhMpldjvY",
+  authDomain: "study-record-8dd51.firebaseapp.com",
+  projectId: "study-record-8dd51",
+  storageBucket: "study-record-8dd51.appspot.com",
+  messagingSenderId: "24057415940",
+  appId: "1:24057415940:web:241cd27184537a74aae851"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
